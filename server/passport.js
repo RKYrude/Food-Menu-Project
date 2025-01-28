bf2e46192135e7c672772bd5aeb3746083f07a1b
Sourashish Das <rickydasrude@gmail.com>
2025-01-28 23:08:55 +0530

    little change

--- server/passport.js
+++ server/passport.js
@@ -1,7 +1,7 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import dotenv from "dotenv";
-import db from "./database";
+import db from "./database.js";
 
 dotenv.config();
 
